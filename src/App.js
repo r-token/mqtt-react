688e749fa6ae8e37d473e98e7241c010a7aff0be
Ryan Token <ryantoken13@gmail.com>
2020-08-04 12:04:22 -0500

    Added step-by-step documentation to the README and added some comments to App.js

--- src/App.js
+++ src/App.js
@@ -3,40 +3,37 @@ import './App.css';
 
 var mqtt = require('mqtt');
 var options = {
-  protocol: 'mqtts',
-  // clientId uniquely identifies client
-  // choose any string you wish
+    // mqtts = mqtt, but secured with SSL/TLS (like https)
+  protocol: 'mqtts', 
+    // clientId uniquely identifies client
+    // choose any string you wish
   clientId: 'b0908853'
 };
+
+// a test mqtt broker you can use
 var client = mqtt.connect('mqtt://test.mosquitto.org:8081', options);
 
-// preciouschicken.com is the MQTT topic
-client.subscribe('ryanstopic1994scout');
+// testtopic1234 is the MQTT topic - you can call this whatever you want
+client.subscribe('testtopic1234');
 
 function App() {
   var note;
   client.on('message', function (topic, message) {
     note = message.toString();
-    // Updates React state with message 
+      // Updates React state with message 
     setMesg(note);
     console.log(note);
     client.end();
   });
 
   // Sets default React state 
-  const [mesg, setMesg] = useState(<Fragment><em>nothing heard</em></Fragment>);
+  const [mesg, setMesg] = useState(<Fragment><em>No messages detected...</em></Fragment>);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>A taste of MQTT in React</h1>
-        <p>The message is: {mesg}</p>
-        <p>
-          <a href="https://www.preciouschicken.com/blog/posts/a-taste-of-mqtt-in-react/"
-            style={{
-              color: 'white'
-            }}>preciouschicken.com/blog/posts/a-taste-of-mqtt-in-react/</a>
-        </p>
+        <p>Message: {mesg}</p>
       </header>
     </div>
   );
